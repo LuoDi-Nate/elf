2a57f11935b357f5778773ae99ff4ec7929f9771
LuoDi-Nate <543667537@qq.com>
2015-09-06 22:24:47 +0800

    add some util

---
+++ elf-common/src/main/java/com/fooluodi/elf/common/exception/BaseExceptions.java
@@ -0,0 +1,9 @@
+package com.fooluodi.elf.common.exception;
+
+public enum BaseExceptions implements IExceptionCode {
+	@Desc("未知异常")
+	UNKNOWN_EXCEPTION,
+
+	@Desc("用户认证错误:用户登录失败")
+	SESSION_VALIDATE_EXCEPTION
+}
---
+++ elf-common/src/main/java/com/fooluodi/elf/common/exception/ExceptionHelper.java
@@ -0,0 +1,29 @@
+package com.fooluodi.elf.common.exception;
+
+import java.lang.reflect.Field;
+
+public class ExceptionHelper {
+	public static String getMessage(IExceptionCode code) {
+		if (code == null)
+			code = BaseExceptions.UNKNOWN_EXCEPTION;
+		try {
+			Field field = code.getClass().getField(code.name());
+			Desc descAnnotation = field.getAnnotation(Desc.class);
+			if (descAnnotation == null)
+				return code.name();
+			else if (descAnnotation.value() == null || "".equals(descAnnotation.value().trim()))
+				return code.name();
+			else
+				return descAnnotation.value();
+		} catch (Throwable e) {
+			return code.name();
+		}
+	}
+
+	public static String getCode(IExceptionCode code) {
+		if (code == null)
+			return BaseExceptions.UNKNOWN_EXCEPTION.name();
+		else
+			return code.name();
+	}
+}
