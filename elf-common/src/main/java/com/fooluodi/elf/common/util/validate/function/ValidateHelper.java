06b182e86ec2151a31f5ad059bc4dab52118b7f8
diwa <di@diwas-MacBook-Pro.local>
2016-07-22 11:15:59 +0800

    add annotation

--- elf-common/src/main/java/com/fooluodi/elf/common/util/validate/function/ValidateHelper.java
+++ elf-common/src/main/java/com/fooluodi/elf/common/util/validate/function/ValidateHelper.java
@@ -47,6 +47,8 @@ public class ValidateHelper {
     /**
      * a hash map to cache class 's structure
      * when a new bean call this util , the structure of this bean will be cached
+     *
+     * 感谢政委的想法, 特此加一行注释
      */
     private static final Map<Class, Map<Class, Set<Field>>> classStructureCache = new HashMap<Class, Map<Class, Set<Field>>>();
 
